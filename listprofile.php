5f714c06304593e0faa107704f400111a557c25f
Michael ECKHARDT <meckhard@e5r7p4.jnb.42.fr>
2016-11-04 09:18:37 +0200

    profile listing

--- listprofile.php
+++ listprofile.php
@@ -1,70 +1,53 @@
  <?php    
     
-    function imagelist($rel, $urls, $id, $liclass, $imgclass)
+    function imagelist($username, $firstname, $lastname, $imgurl, $age, $bio)
     { 
-       // echo "<ul class='hoverbox'>";
-        $count = count($urls);
+        $count = count($username);
        for($i = 0 ; $i < $count; $i++)
         {
             echo"
-            <li class='$liclass'>
-            <a href='" . $rel . $id[$i]
-            . "'><img src='" . $rel . $urls[$i] . "'
-            alt='image not found'
-            class='" . $imgclass . "'/></a>
-            </li>";
-        }
-        //echo "</ul>";
-
-
-
-
-
-
-
-        <div class="container">
-    <div class="user-menu-container  "> <!--  square row-->
-        <div class="col-md-7 user-details">
-            <div class="row coralbg white">
-                <div class="col-md-6 no-pad">
-                    <div class="user-pad">
-                        <h3>$firstname $lastname</h3>
-                        <h4 class="white"><i class="fa fa-check-circle-o"></i> San Antonio, TX</h4>
-                        <h4 class="white"><i class="fa fa-twitter"></i> CoolesOCool</h4>
-                      
-                        <button type="button" class="btn btn-info" href="#">
-                        <span><i class="glyphicon glyphicon-user"></i></span></button>
-                        <button type="button" class=" btn btn-info" href="#">
-                        <span><i class="glyphicon glyphicon-comment"></i></span></button>
-                          <button data-dismiss="modal" data-toggle="modal" data-target="#chat-modal" type="button" class="btn  btn-info" href="#">
-                        <span><i class="glyphicon glyphicon-thumbs-up"></i></span></button>
+                        <div class='container'>
+    <div class='user-menu-container'>
+        <div class='col-md-7 user-details'>
+            <div class='row coralbg white'>
+                <div class='col-md-6 no-pad'>
+                    <div class='user-pad'>
+                        <h3>$firstname[$i] $lastname[$i]</h3>
+                        <h4 class='white'><i class='fa fa-check-circle-o'></i>$location[$i]</h4>
+                         <h4 class='white'><i class='fa fa-check-circle-o'></i>$age[$i]</h4>
+                        <button type='button' class='btn btn-info' href='#'>
+                        <span><i class='glyphicon glyphicon-user'></i></span></button>
+                        <button type='button' class=' btn btn-info' href='#'>
+                        <span><i class='glyphicon glyphicon-comment'></i></span></button>
+                          <button data-dismiss='modal' data-toggle='modal' data-target='#chat-modal' type='button' class='btn  btn-info' href='#'>
+                        <span><i class='glyphicon glyphicon-thumbs-up'></i></span></button>
                     </div>
                 </div>
-                <div class="col-md-6 no-pad">
-                    <div class="user-image">
-                        <img src="$urls" class="img-responsive thumbnail">
+                <div class='col-md-6 no-pad'>
+                    <div class='user-image'>
+                        <img src='$imgurl[$i]' class='img-responsive thumbnail'>
                     </div>
                 </div>
             </div>
-              <h3>Interests/ bio ¯\_(ツ)_/¯</h3>
-     <!--       <div class="row overview">
+              <h3>$bio[$i]</h3>
+     <!--       <div class='row overview'>
   
             </div> -->
         </div>
    
     </div>
-</div>
+</div>";
+        }
     }
 
-
-    function extract_image_user()
+  function extract_users()
     {
         session_start();
-        $user = $_SESSION['logged_on_user'];
+        /*$user = $_SESSION['logged_on_user']; */
         $pdo = connect();
         $pdo->query("USE db_matcha");
         echo $user;
-        $stmt = $pdo->prepare("SELECT `image_url` FROM `imagetable` WHERE `user` = '$user' ORDER BY `date_created` DESC ");
+        $stmt = $pdo->prepare("SELECT `usernames` FROM `imagetable` WHERE `user` = '$user'");
         $stmt->execute();   
         $urls = $stmt->fetchAll(PDO::FETCH_COLUMN);
         $pdo = null;
@@ -74,11 +57,39 @@
     function extract_image_user()
     {
         session_start();
-        $user = $_SESSION['logged_on_user'];
+        /*$user = $_SESSION['logged_on_user']; */
         $pdo = connect();
         $pdo->query("USE db_matcha");
         echo $user;
-        $stmt = $pdo->prepare("SELECT `image_url` FROM `imagetable` WHERE `user` = '$user' ORDER BY `date_created` DESC ");
+        $stmt = $pdo->prepare("SELECT `image_url` FROM `imagetable` WHERE `user` = '$user'");
+        $stmt->execute();   
+        $urls = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $pdo = null;
+        return ($urls);
+    }
+
+    function extract_age_user()
+    {
+        session_start();
+        /*$user = $_SESSION['logged_on_user']; */
+        $pdo = connect();
+        $pdo->query("USE db_matcha");
+        echo $user;
+        $stmt = $pdo->prepare("SELECT `age` FROM `agetable` WHERE `user` = '$user'");
+        $stmt->execute();   
+        $urls = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $pdo = null;
+        return ($urls);
+    }
+
+    function extract_location_user()
+    {
+        session_start();
+        /*$user = $_SESSION['logged_on_user']; */
+        $pdo = connect();
+        $pdo->query("USE db_matcha");
+        echo $user;
+        $stmt = $pdo->prepare("SELECT `location` FROM `locationtable` WHERE `user` = '$user'");
         $stmt->execute();   
         $urls = $stmt->fetchAll(PDO::FETCH_COLUMN);
         $pdo = null;

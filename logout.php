685bc40f5a053ee06f94870180e5e5edaf5585eb
Luke HAMLYN <lhamlyn@e5r7p5.jnb.42.fr>
2016-11-13 12:19:11 +0200

    logging out works

--- logout.php
+++ logout.php
@@ -1,16 +1,12 @@
    <?php
         include"bckend/connect.php";
-        session_start();
-        date_default_timezone_set("Africa/Johannesburg");
-        $date = date("d/m/Y G:i:s");
-        $user = $_SESSION['logged_on_user'];
+        //session_start();
+        $user = /*$_SESSION['logged_on_user']*/'loowen';
         $pdo=connect();
         $pdo->query("USE matcha_db");
-        $stmt = $pdo;
-        $stmt->query("UPDATE `users` SET `LastLog` = $date AND `Logged` = FALSE WHERE `Username` = :user");
+        $stmt = $pdo->prepare("UPDATE `users` SET `LastLog` = NOW(), `Logged` = 0 WHERE `Username` = :user");
         $stmt->bindParam(":user", $user);
         $stmt->execute();
         $_SESSION['logged_on_user'] = "";
-        header("Location: index.php");
-
+        echo ' <script type="text/javascript"> parent.window.location.replace("http://localhost:8080/Matcha/login.php"); </script> ';
     ?>
